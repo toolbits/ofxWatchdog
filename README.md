54c84afeff4622b20b3591ae1dcadddb76f77f60
toolbits <zap00365@nifty.com>
2016-02-21 17:39:05 +0900

    support openFrameworks 0.9.2

--- README.md
+++ README.md
@@ -15,7 +15,8 @@ Optionally you can use:<br/>
 o ofxWatchdog::clear(void)<br/>
 <br/>
 Development environment:<br/>
-o &lt;NEW!> MacOS X 10.10.3 + Xcode 6.3.1 + of 0.8.4 osx<br/>
+o &lt;NEW!> MacOS X 10.11.3 + Xcode 6.2 + of 0.9.2 osx<br/>
+o MacOS X 10.10.3 + Xcode 6.3.1 + of 0.8.4 osx<br/>
 o ubuntu linux 14.04 LTS + Code::Blocks 13.12 + of 0.8.4 linux 64<br/>
 o MacOS X 10.10 + Xcode 6.1 + of 0.8.4 osx<br/>
 o MacOS X 10.9.4 + Xcode 5.1.1 + of 0.8.3 osx<br/>
--- example_hangupShutdownExample/src/main.cpp
+++ example_hangupShutdownExample/src/main.cpp
@@ -4,6 +4,9 @@
 
 //========================================================================
 int main(){
+    
+    ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+    
     // ofxWatchdog::watch(msec, reboot, override, verbose)
     //
     //     msec : how long does the watchdog wait, when the process hangs-up in milli seconds
@@ -12,8 +15,6 @@ int main(){
     //  verbose : print more log information (optional)
     
     ofxWatchdog::watch(3000, false, true, true);
-    
-    ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
     
     // this kicks off the running of my app
     // can be OF_WINDOW or OF_FULLSCREEN
